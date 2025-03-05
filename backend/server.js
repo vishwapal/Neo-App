d0b094e3244e1e7e996788903aa3483255f9c85f
vishwapal <vkorde940@gmail.com>
2025-03-05 15:49:28 +0530

    fixed corse issue on frontend

--- backend/server.js
+++ backend/server.js
@@ -16,10 +16,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Set the correct path for `dist` (located at the root level of `Neo-App`)
-app.use(express.static(path.join(__dirname, "../dist")));
-
+app.use(express.static(path.join(__dirname, "../dist"))); // Adjust path
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+  res.sendFile(path.join(__dirname, "../dist", "index.html")); // Adjust path
 });
 
 const PORT = process.env.PORT || 5000;
